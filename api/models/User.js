90e545a7cd967682929d4a380f56227491685f66
Ngoc <109322822+Ngochb2809@users.noreply.github.com>
2024-03-08 14:44:13 +0700

    Delete api directory

--- api/models/User.js
+++
@@ -1,8 +0,0 @@
-const monggose = require('mongoose');
-const {Schema, model} = monggose;
-const UserSvhema = new Schema({
-    username: {type: 'string', required: true, min:4, unique: true},
-    password: {type: 'string', required: true}
-});
-const UserModel = model('User', UserSvhema)
-module.exports = UserModel;
