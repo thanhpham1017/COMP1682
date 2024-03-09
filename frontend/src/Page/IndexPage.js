4bec7690533fd941d6b1e0a8244f76144408c52a
Ngochb2809 <ngochbgbh211287@fpt.edu.vn>
2024-03-10 01:08:47 +0700

    Front-end login

---
+++ frontend/src/Page/IndexPage.js
@@ -0,0 +1,12 @@
+import Post from "../Post";
+
+export default function IndexPage(){
+    return(
+        <>
+            <Post/>
+            <Post/>
+        </>
+        
+    );
+    //mongoose.connect('mongodb+srv://foodmap:webne123@cluster0.escyyuf.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0');
+}
