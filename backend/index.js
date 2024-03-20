a03bcc23f8245f755dc03edb649a4291bfe5d9e3
unknown <thanhpqgch210568@fpt.edu.vn>
2024-03-20 08:23:44 +0700

    detail,edit

--- backend/index.js
+++ backend/index.js
@@ -1,15 +1,16 @@
 const express = require('express');
 const cors = require('cors');
-const mongoose = require('mongoose');
-const bcrypt = require('bcryptjs');
+const mongoose = require("mongoose");
 const User = require('./models/User');
 const Post = require('./models/Post');
+const bcrypt = require('bcryptjs');
 const app = express();
 const jwt = require('jsonwebtoken');
 const cookieParser = require('cookie-parser');
 const multer = require('multer');
-const uploadMiddleware = multer({ dest: 'uploads/'});
+const uploadMiddleware = multer({ dest: 'uploads/' });
 const fs = require('fs');
+
 
 
 const salt = bcrypt.genSaltSync(10);
@@ -87,14 +88,52 @@ app.post('/post', uploadMiddleware.single('file'), async (req,res) => {
 
 });
 
-app.get('/post', async (req, res) => {
-  res.json(await Post.find()
-    .populate('author', ['username'])
-    .sort({createdAt: -1})
-    .limit(20)
-    );
+app.put('/post',uploadMiddleware.single('file'), async (req,res) => {
+  let newPath = null;
+  if (req.file) {
+    const {originalname,path} = req.file;
+    const parts = originalname.split('.');
+    const ext = parts[parts.length - 1];
+    newPath = path+'.'+ext;
+    fs.renameSync(path, newPath);
+  }
+
+  const {token} = req.cookies;
+  jwt.verify(token, secret, {}, async (err,info) => {
+    if (err) throw err;
+    const {id,title,summary,content} = req.body;
+    const postDoc = await Post.findById(id);
+    const isAuthor = JSON.stringify(postDoc.author) === JSON.stringify(info.id);
+    if (!isAuthor) {
+      return res.status(400).json('you are not the author');
+    }
+    
+    await postDoc.updateOne({
+      title,
+      summary,
+      content,
+      cover: newPath ? newPath : postDoc.cover,
+    });
+
+    res.json(postDoc);
+  });
+
 });
 
+app.get('/post', async (req,res) => {
+  res.json(
+    await Post.find()
+      .populate('author', ['username'])
+      .sort({createdAt: -1})
+      .limit(20)
+  );
+});
+
+app.get('/post/:id', async (req, res) => {
+  const {id} = req.params;
+  const postDoc = await Post.findById(id).populate('author', ['username']);
+  res.json(postDoc);
+})
 
 
 app.listen(4000);
--- backend/models/Post.js
+++ backend/models/Post.js
@@ -2,13 +2,13 @@ const mongoose = require('mongoose');
 const {Schema,model} = mongoose;
 
 const PostSchema = new Schema({
-    title:String,
-    summary:String,
-    content:String,
-    cover:String,
-    author:{type:Schema.Types.ObjectId, ref:'User'},
+  title:String,
+  summary:String,
+  content:String,
+  cover:String,
+  author:{type:Schema.Types.ObjectId, ref:'User'},
 }, {
-    timestamps: true,
+  timestamps: true,
 });
 
 const PostModel = model('Post', PostSchema);
