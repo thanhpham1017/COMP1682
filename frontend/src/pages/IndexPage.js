11e6e9d71a5f97e68a1664b4451beca15fa765e9
Ngochb2809 <ngochbgbh211287@fpt.edu.vn>
2024-04-10 00:57:21 +0700

    front end side bar

--- frontend/src/pages/IndexPage.js
+++ frontend/src/pages/IndexPage.js
@@ -5,209 +5,231 @@ import {UserContext} from "./UserContext";
 import { GeolocateControl } from 'react-map-gl';
 
 // import axios from "axios";
-import { FaMapMarker,FaStar  } from 'react-icons/fa';
+import { FaMapMarker,FaStar,FaTimes   } from 'react-icons/fa';
 
 export default function IndexPage(){
-    const [posts,setPosts] = useState([]);
-    const [pins, setPins] = useState([]);
-    const [currentPlaceId, setCurrentPlaceId] = useState(null);
-    const [selectedPin, setSelectedPin] = useState(null);
-    const [newPlace, setNewPlace] = useState(null);
-    const [title, setTitle] = useState(null);
-    const [desc, setDesc] = useState(null);
-    const [price, setPrice] = useState(0);
-    const [star, setStar] = useState(0);
-    const {userInfo } = useContext(UserContext);
-    const [viewport, setViewport] = useState({
+  const [posts,setPosts] = useState([]);
+  const [pins, setPins] = useState([]);
+  const [currentPlaceId, setCurrentPlaceId] = useState(null);
+  const [selectedPin, setSelectedPin] = useState(null);
+  const [newPlace, setNewPlace] = useState(null);
+  const [title, setTitle] = useState(null);
+  const [desc, setDesc] = useState(null);
+  const [price, setPrice] = useState(0);
+  const [star, setStar] = useState(0);
+  const {userInfo } = useContext(UserContext);
+  const [selectedMarkerInfo, setSelectedMarkerInfo] = useState(null);
+  const [viewport, setViewport] = useState({
+    zoom: 13,
+  });
+  const GeolocateController = useRef();
+  const [isMarkerSelected, setIsMarkerSelected] = useState(false);
 
-      zoom: 13,
-    });
-    const GeolocateController = useRef();
-
-    useEffect(() => {
-        fetch('http://localhost:4000/post').then(reponse => {
-            reponse.json().then(posts => {
-                setPosts(posts);
-            });
-        });
-    }, []);
-
-    
-
-    useEffect(() => {
-      console.log("pins:", pins);
-      console.log("currentPlaceId:", currentPlaceId);
-      const getPins = async () => {
-        try {    
-          const response = await fetch("http://localhost:4000/pins"); // Đổi lại địa chỉ máy chủ và tên tuyến đường cần truy cập
-          if (!response.ok) {
-            throw new Error('Network response was not ok');
-          }
-          const pinsData = await response.json();
-          setPins(pinsData);
-        } catch (error) {
-          console.error('There was a problem with the fetch operation:', error);
-        }
-      };
-      getPins();
-
-      GeolocateControl.current?.trigger()
-    }, [currentPlaceId, pins, viewport, GeolocateController]);
-    
-    const handleMarkerClick = (id,lat,long) => {
-        console.log("Marker clicked with id:", id);
-        setSelectedPin(pins.find(pin => pin._id === id));
-        setCurrentPlaceId(id);
-        setViewport({ ...viewport, latitude: lat, longitude: long });
-    }
-
-    const handleAddClick = (e) => {
-      const long = e.lngLat.lng;
-      const lat = e.lngLat.lat;
-      setNewPlace({
-        lat,
-        long,
+  useEffect(() => {
+      fetch('http://localhost:4000/post').then(reponse => {
+          reponse.json().then(posts => {
+              setPosts(posts);
+          });
       });
-    }
-    const currentUser = userInfo?.username;
+  }, []);
 
-    const handleSubmit = async (e) => {
-      e.preventDefault();
-      const newPin = {
-        username: currentUser,
-        title,
-        desc,
-        price,
-        rating: star,
-        lat: newPlace.lat,
-        long: newPlace.long,
-      };
-    
-      try { 
-        const response = await fetch("http://localhost:4000/pinCreate", {
-          method: 'POST',
-          headers: {
-            'Content-Type': 'application/json',
-          },
-          body: JSON.stringify(newPin),
-        });
-    
+  useEffect(() => {
+    const getPins = async () => {
+      try {    
+        const response = await fetch("http://localhost:4000/pins");
         if (!response.ok) {
           throw new Error('Network response was not ok');
         }
-    
-        const pinData = await response.json();
-        setPins([...pins, pinData]);
-        setNewPlace(null);
+        const pinsData = await response.json();
+        setPins(pinsData);
       } catch (error) {
         console.error('There was a problem with the fetch operation:', error);
       }
-    }; 
-    return(
-      <div>
-          <MapGL
-            mapboxAccessToken={process.env.REACT_APP_MAPBOX}
-            initialViewState={{
+    };
+    getPins();
+
+    GeolocateControl.current?.trigger()
+  }, [currentPlaceId, pins, viewport, GeolocateController]);
+  
+  const handleMarkerClick = (id, lat, long) => {
+    setSelectedPin(pins.find(pin => pin._id === id));
+    setCurrentPlaceId(id);
+    setViewport({ ...viewport, latitude: lat, longitude: long });
+    setSelectedMarkerInfo(pins.find(pin => pin._id === id));
+    setIsMarkerSelected(true);
+  }
+
+  const handleAddClick = (e) => {
+    const long = e.lngLat.lng;
+    const lat = e.lngLat.lat;
+    setNewPlace({
+      lat,
+      long,
+    });
+  }
+  const currentUser = userInfo?.username;
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const newPin = {
+      username: currentUser,
+      title,
+      desc,
+      price,
+      rating: star,
+      lat: newPlace.lat,
+      long: newPlace.long,
+    };
+  
+    try { 
+      const response = await fetch("http://localhost:4000/pinCreate", {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(newPin),
+      });
+  
+      if (!response.ok) {
+        throw new Error('Network response was not ok');
+      }
+  
+      const pinData = await response.json();
+      setPins([...pins, pinData]);
+      setNewPlace(null);
+    } catch (error) {
+      console.error('There was a problem with the fetch operation:', error);
+    }
+  }; 
+
+  const handleCloseSidebar = () => {
+    setIsMarkerSelected(false); // Đặt trạng thái marker đã được chọn là false khi người dùng đóng side bar
+  };
+
+  return(
+    <div style={{position: "relative"}}>
+        <MapGL
+          mapboxAccessToken={process.env.REACT_APP_MAPBOX}
+          initialViewState={{
             ...viewport
-            }}
-            style={{width: 1750, height: 500}}
-            mapStyle="mapbox://styles/mapbox/streets-v9"
-            onDblClick={handleAddClick}
-            transitionDuration = "200"
-          >
-            <GeolocateControl positionOptions={{ enableHighAccuracy: true }} trackUserLocation={true} ref = {GeolocateController} />
-            {pins.map(p => (
-              <>
-                <Marker 
-                  longitude={p.long} 
-                  latitude={p.lat} 
-                  anchor="bottom" 
-                >
-                  <FaMapMarker 
-                    style={{
-                      fontSize: 300 / viewport.zoom, // Tính toán kích thước dựa trên zoom
-                      color:"red",
-                      cursor: "pointer",
-                    }}
-                    onClick={() => handleMarkerClick(p._id,p.lat,p.long) }
-                  />
-
-                </Marker>
-
-                  <Popup 
-                    key={currentPlaceId}
-                    longitude={selectedPin &&selectedPin.long} 
-                    latitude={selectedPin &&selectedPin.lat}
-                    anchor="left"
-                    onClose={() => setCurrentPlaceId(null)}
-                  >
-                    {console.log("Popup is rendered")} {/* In ra console để kiểm tra xem Popup có được render hay không */}
-                    <div className="card">
-                      <label>{selectedPin && selectedPin.title}</label><br />
-                      <label>{selectedPin && selectedPin.desc}</label><br />
-                      <label>{selectedPin && selectedPin.price}</label><br />
-                      <div>
-                          <a href="link-to-youtube">Youtube</a> // <a href="link-to-instagram">Instagram</a> // <a href="link-to-tiktok">Tiktok</a>
-                      </div>
-                      <div className="ratings">
-                          {Array(selectedPin && selectedPin.rating).fill(<FaStar className="star" />)}
-                          <label>{selectedPin && selectedPin.rating}</label>
-                      </div>
-                      <label>{selectedPin && selectedPin.username}</label><br />
+          }}
+          style={{width: "100%", height: "500px"}}
+          mapStyle="mapbox://styles/mapbox/streets-v9"
+          onDblClick={handleAddClick}
+          transitionDuration="200"
+        >
+          <GeolocateControl positionOptions={{ enableHighAccuracy: true }} trackUserLocation={true} ref={GeolocateController} />
+          {pins.map(p => (
+            <Marker 
+              longitude={p.long} 
+              latitude={p.lat} 
+              anchor="bottom" 
+              key={p._id}
+            >
+              <FaMapMarker 
+                style={{
+                  fontSize: 300 / viewport.zoom,
+                  color:"red",
+                  cursor: "pointer",
+                }}
+                onClick={() => handleMarkerClick(p._id,p.lat,p.long) }
+              />
+            </Marker>
+          ))}
+          {newPlace && (
+            <div className="sidebar" style={{position: "absolute", top: 0, right: 0, width: "460px", height: "460px", backgroundColor: "#fff", boxShadow: "-2px 0 5px rgba(0, 0, 0, 0.1)", zIndex: 1000, overflowY: "auto"}}>
+              <button onClick={() => setNewPlace(null)} style={{background: "none", border: "none", cursor: "pointer", position: "absolute", top: "10px", right: "10px"}}>
+                <FaTimes style={{fontSize: "1.5rem"}} />
+              </button>
+              <form onSubmit={handleSubmit}>
+                <div className="formCard">
+                  <label>Name:</label><br />
+                  <input 
+                    className="formInput" 
+                    placeholder="Enter name" 
+                    onChange={(e) => setTitle(e.target.value)}
+                  /><br />
+                  <label>Review:</label><br />
+                  <textarea className="formInput" 
+                    placeholder="Say something about this place"
+                    onChange={(e) => setDesc(e.target.value)}
+                  ></textarea><br />
+                  <label>Price:</label><br />
+                  <input 
+                    className="formInput" 
+                    type="number" 
+                    placeholder="Price" 
+                    onChange={(e) => setPrice(e.target.value)}
+                  /><br />
+                  <div>
+                    <a href="link-to-youtube">Youtube</a> // <a href="link-to-instagram">Instagram</a> // <a href="link-to-tiktok">Tiktok</a>
                   </div>
-                  </Popup>
-              </>
-            ))}
-            {newPlace && 
-              <Popup 
-                    longitude={newPlace.long} 
-                    latitude={newPlace.lat}
-                    anchor="left"
-                    onClose={() => setNewPlace(null)}
-                  >
-                  <form onSubmit={handleSubmit}>
-                    <div className="formCard">
-                      <label>Name:</label><br />
-                      <input 
-                        className="formInput" 
-                        placeholder="Enter name" 
-                        onChange={(e) => setTitle(e.target.value)}
-                        /><br />
-                      <label>Review:</label><br />
-                      <textarea className="formInput" 
-                        placeholder="Say something about this place"
-                        onChange={(e) => setDesc(e.target.value)}
-                      ></textarea><br />
-                      <label>Price:</label><br />
-                      <input 
-                        className="formInput" 
-                        type="number" 
-                        placeholder="Price" 
-                        onChange={(e) => setPrice(e.target.value)}
-                        /><br />
-                      <div>
-                        <a href="link-to-youtube">Youtube</a> // <a href="link-to-instagram">Instagram</a> // <a href="link-to-tiktok">Tiktok</a>
-                      </div>
-                      <label>Rating:</label><br />
-                      <select className="formInput" onChange={(e) => setStar(e.target.value)}>
-                        <option value="1">1</option>
-                        <option value="2">2</option>
-                        <option value="3">3</option>
-                        <option value="4">4</option>
-                        <option value="5">5</option>
-                      </select><br />
-                      <button className="formSubmitButton" type="submit">Add Pin</button><br />
-                    </div>
-                  </form>
-              </Popup>      
-            } 
+                  <label>Rating:</label><br />
+                  <select className="formInput" onChange={(e) => setStar(e.target.value)}>
+                    <option value="1">1</option>
+                    <option value="2">2</option>
+                    <option value="3">3</option>
+                    <option value="4">4</option>
+                    <option value="5">5</option>
+                  </select><br />
+                  <button className="formSubmitButton" type="submit">Add Pin</button><br />
+                </div>
+              </form>
+            </div>
+          )}
         </MapGL>
+        {isMarkerSelected && (
+          <div className="sidebar" style={{position: "absolute", top: 0, right: 0, width: "300px", height: "460px", backgroundColor: "#fff", boxShadow: "-2px 0 5px rgba(0, 0, 0, 0.1)", zIndex: 1000, overflowY: "auto"}}>
+            <button onClick={handleCloseSidebar} style={{background: "none", border: "none", cursor: "pointer", position: "absolute", top: "10px", right: "10px"}}>
+              <FaTimes style={{fontSize: "1.5rem"}} />
+            </button>
+            {selectedMarkerInfo && (
+              <div className="marker-info">
+                <h2>{selectedMarkerInfo.title}</h2>
+                <p>{selectedMarkerInfo.desc}</p>
+                <p>Price: {selectedMarkerInfo.price}</p>
+                <div>
+                  <a href="link-to-youtube">Youtube</a> // <a href="link-to-instagram">Instagram</a> // <a href="link-to-tiktok">Tiktok</a>
+                </div>
+                <div className="ratings">
+                  {Array(selectedMarkerInfo.rating).fill(<FaStar className="star" />)}
+                </div>
+                <p>Posted by: {selectedMarkerInfo.username}</p>
+              </div>
+            )}
+          </div>
+        )}
         <h1>Blog</h1>
         <div className="post-wrapper">
-            {posts.length > 0 && posts.map((post) => (
-              <Post key={post.id} {...post} />
-            ))}
+          {posts.length > 0 && posts.map((post) => (
+            <Post key={post.id} {...post} />
+          ))}
         </div>
-      </div>
-    );
+    </div>
+  );
 }
+
+
+
+// {/* <Popup 
+//                     key={currentPlaceId}
+//                     longitude={selectedPin &&selectedPin.long} 
+//                     latitude={selectedPin &&selectedPin.lat}
+//                     anchor="left"
+//                     onClose={() => setCurrentPlaceId(null)}
+//                   >
+//                     {console.log("Popup is rendered")} {/* In ra console để kiểm tra xem Popup có được render hay không */}
+//                     <div className="card">
+//                       <label>{selectedPin && selectedPin.title}</label><br />
+//                       <label>{selectedPin && selectedPin.desc}</label><br />
+//                       <label>{selectedPin && selectedPin.price}</label><br />
+//                       <div>
+//                           <a href="link-to-youtube">Youtube</a> // <a href="link-to-instagram">Instagram</a> // <a href="link-to-tiktok">Tiktok</a>
+//                       </div>
+//                       <div className="ratings">
+//                           {Array(selectedPin && selectedPin.rating).fill(<FaStar className="star" />)}
+//                           {/*<label>{selectedPin && selectedPin.rating}</label>*/}
+//                       </div>
+//                       <label>{selectedPin && selectedPin.username}</label><br />
+//                   </div>
+//                   </Popup> */}
