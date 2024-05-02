d3d9261235232c53112555f6c3f93f9cec9961bc
unknown <thanhpqgch210568@fpt.edu.vn>
2024-05-02 22:33:30 +0700

    fix

--- backend/index.js
+++ backend/index.js
@@ -56,7 +56,7 @@ if(process.env.NODE_ENV === 'production') {
     app.use(express.static(path.join(__dirname1, '/frontend/build')));
 
     app.get('*', (req, res) => {
-        res.sendFile(path.resolve(__dirname1, 'front-end', 'build', 'index.html'));
+        res.sendFile(path.resolve(__dirname1, 'frontend', 'build', 'index.html'));
     });
 } else {
     app.get('/', (req, res) => {
